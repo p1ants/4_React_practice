32ef1da2e3b85fcf28639a2be0d51749f28df0de
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-01-05 17:39:12 +0900

    숙제끝

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,34 @@
 // src/pages/Work.js
-
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
+
+const StBox = styled.div`
+  max-width: 1200px;
+  width: 90%;
+  height: 1000px;
+  border: 1px solid gray;
+  border-radius: 15px;
+  margin: 20px 20px 20px 20px;
+`;
+
+const StBox_top = styled.div`
+  display: flex;
+
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const StContainer = styled.div`
+  border: 2px solid #eee;
+  width: 99.5%;
+  height: 900px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 function Detail() {
   // const dispatch = useDispatch()
@@ -12,14 +38,17 @@ function Detail() {
   const work = todoList.find((work) => work.id === parseInt(param.id));
   // dispatch()
   return (
-    <div>
-      <div>
+    <StBox>
+      <StBox_top>
         <span>ID:{work.id}</span>
         <button onClick={() => navigate('/')}>이전으로</button>
-      </div>
-      <div>{work.title}</div>
-      <div>{work.content}</div>
-    </div>
+      </StBox_top>
+
+      <StContainer>
+        <h1>{work.title}</h1>
+        <div>{work.content}</div>
+      </StContainer>
+    </StBox>
   );
 }
 
